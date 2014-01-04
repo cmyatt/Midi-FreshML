b17cb553201ca848939e4adec135e6e6032ac4d2
Chris Myatt <cgm30@cam.ac.uk>
2014-01-04 00:43:32 +0000

    Implemented Match typechecking, only Let still to do

--- test.ml
+++ test.ml
@@ -1,50 +1,64 @@
+open AbSyn;;
 
-let ys = [AbSyn.Ns(["a";"b";"c"]);
-          AbSyn.Ds(["exp";"typ"], [("Lam", [AbSyn.IntT; AbSyn.DataT("exp")])])];;
+let ys = [Ns(["a";"b";"c"]);
+          Ds(["exp";"typ"], [("Lam", [IntT; DataT("exp")])])];;
 
 TyCheck.init ys;;
 
 let test (e, p) =
-    try (print_string ((AbSyn.string_of_expr e)^" : ");
-         print_string ((AbSyn.stringify (TyCheck.get_type [] [] (e,p))^"\n"))) with
+    try (print_string ((string_of_expr e)^" : ");
+         print_string ((string_of_typ (TyCheck.get_type [] [] (e,p))^"\n"))) with
         TyCheck.Type_error(s) -> print_string (s^"\n");;
 
 print_string "\n";;
-test (AbSyn.Id("x"), (1,0));;
-test (AbSyn.Ctor("App", (AbSyn.RealLiteral(3.), (5, 0))), (2,0));;
-test (AbSyn.Ctor("Lam", (AbSyn.IntLiteral(3), (5, 0))), (3,0));;
-test (AbSyn.Fresh("a"), (4, 0));;
-test (AbSyn.If(
-    (AbSyn.Fresh("b"), (0, 1)),
-    (AbSyn.Fresh("b"), (0, 2)),
-    (AbSyn.RealLiteral(0.), (0, 3)),
-    (AbSyn.RealLiteral(1.), (0, 4))
+test (Id("x"), (1,0));;
+test (Ctor("App", (RealLiteral(3.), (5, 0))), (2,0));;
+test (Ctor("Lam", (IntLiteral(3), (5, 0))), (3,0));;
+test (Fresh("a"), (4, 0));;
+test (If(
+    (Fresh("b"), (0, 1)),
+    (Fresh("b"), (0, 2)),
+    (RealLiteral(0.), (0, 3)),
+    (RealLiteral(1.), (0, 4))
 ), (5, 0));;
-test (AbSyn.If(
-    (AbSyn.Fresh("a"), (0, 1)),
-    (AbSyn.Fresh("a"), (0, 2)),
-    (AbSyn.StringLiteral("x"), (0, 3)),
-    (AbSyn.StringLiteral("y"), (0, 4))
+test (If(
+    (Fresh("a"), (0, 1)),
+    (Fresh("a"), (0, 2)),
+    (StringLiteral("x"), (0, 3)),
+    (StringLiteral("y"), (0, 4))
 ), (6, 0));;
-test (AbSyn.Swap(
-    (AbSyn.Fresh("c"), (0, 1)),
-    (AbSyn.Fresh("c"), (0, 2)),
-    (AbSyn.IntLiteral(5), (0, 3))
+test (Swap(
+    (Fresh("c"), (0, 1)),
+    (Fresh("c"), (0, 2)),
+    (IntLiteral(5), (0, 3))
 ), (7, 0));;
-test (AbSyn.NameAb(
-    (AbSyn.Fresh("a"), (0, 1)),
-    (AbSyn.BoolLiteral(false), (0, 2))
+test (NameAb(
+    (Fresh("a"), (0, 1)),
+    (BoolLiteral(false), (0, 2))
 ), (8, 0));;
-test (AbSyn.Pair(
-    (AbSyn.Fresh("a"), (0, 1)),
-    (AbSyn.Fresh("c"), (0, 2))
+test (Pair(
+    (Fresh("a"), (0, 1)),
+    (Fresh("c"), (0, 2))
 ), (9, 0));;
-test (AbSyn.Lambda(
+test (Lambda(
     "x",
-    AbSyn.IntT,
-    (AbSyn.Id("x"), (0, 1))
+    IntT,
+    (Id("x"), (0, 1))
 ), (10, 0));;
-test (AbSyn.App(
-    (AbSyn.Lambda("x", AbSyn.IntT, (AbSyn.Id("x"), (1,0))), (0,1)),
-    (AbSyn.IntLiteral(5), (0,2))
+test (App(
+    (Lambda("x", IntT, (Id("x"), (1,0))), (0,1)),
+    (IntLiteral(5), (0,2))
 ), (11, 0));;
+test (Match(
+    (Lambda("x", IntT, (Id("x"), (1,0))), (0,1)),
+    [((CtorP("Lam", IdP("x"))), (Id("x"), (97,2)))]
+), (11, 0));;
+test (Match(
+    (Ctor("Lam", (IntLiteral(6), (2,23))), (1,0)),
+    [((CtorP("Lam", IdP("x"))), (Id("x"), (97,2)))]
+), (11, 0));;
+test (Match(
+  ((NameAb((Fresh("a"), (2,3)), (BoolLiteral(true), (4,5)))), (12,45)),
+  [(NameAbsP("x", IdP("t")), (Id("t"), (2,4)))]
+), (2,90));;
+
